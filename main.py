e357aac270e9b17a90ab8fd6cd8eb3a1b952c5fb
Sasha <alexandersmirnovjr@gmail.com>
2022-12-28 18:56:41 +0500

    Получил ожидаемую зарплату по всем языкам

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import requests
+import os
+from bs4 import BeautifulSoup
 
 
-class ErrRedirection(Exception):
-    pass
+url = 'https://tululu.org/txt.php'
+response= requests.get(url)
 
 for id in range(1, 11):
 
-
+    soup =  BeautifulSoup(response.text, 'lxml')
     payload = {"id": id }
-    filename = f'book_{id}.txt'
+    filename_test = soup.find('h1')
+    filename = f'{filename_test}.txt'
+    file_path = os.path.join('books', filename)
     url = f"https://tululu.org/txt.php"
     response = requests.get(url, params = payload)
     response.raise_for_status()
-    with open(filename, 'wb') as file:
+    with open(filename, 'wb', file_path) as file:
         file.write(response.content)
 def check_for_redirect(response):
     if response.history:
         raise ErrRedirection("Redirection")
+
+
+if __name__ == "__main__":
+    path = os.makedirs('books', exist_ok = True)
