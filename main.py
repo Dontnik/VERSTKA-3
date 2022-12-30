78303eb12e4f65d0cb206833c25c018ddd00b6d1
Sasha <alexandersmirnovjr@gmail.com>
2022-12-30 18:48:28 +0500

    Revert "Получил ожидаемую зарплату по всем языкам"
    This reverts commit e357aac270e9b17a90ab8fd6cd8eb3a1b952c5fb.

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 import requests
-import os
-from bs4 import BeautifulSoup
 
 
-url = 'https://tululu.org/txt.php'
-response= requests.get(url)
+class ErrRedirection(Exception):
+    pass
 
 for id in range(1, 11):
 
-    soup =  BeautifulSoup(response.text, 'lxml')
+
     payload = {"id": id }
-    filename_test = soup.find('h1')
-    filename = f'{filename_test}.txt'
-    file_path = os.path.join('books', filename)
+    filename = f'book_{id}.txt'
     url = f"https://tululu.org/txt.php"
     response = requests.get(url, params = payload)
     response.raise_for_status()
-    with open(filename, 'wb', file_path) as file:
+    with open(filename, 'wb') as file:
         file.write(response.content)
 def check_for_redirect(response):
     if response.history:
         raise ErrRedirection("Redirection")
-
-
-if __name__ == "__main__":
-    path = os.makedirs('books', exist_ok = True)
