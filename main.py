77486624e72a218a5eeb5fbe56862de3fa179066
Sasha <alexandersmirnovjr@gmail.com>
2023-05-19 18:30:53 +0800

    Правки

--- main.py
+++ main.py
@@ -2,21 +2,38 @@ import requests
 from tqdm import tqdm
 import os
 import urllib
+import time
+import logging
+import argparse
 from bs4 import BeautifulSoup
 
 
 class ErrRedirection(Exception):
     pass
+
+
 def check_for_redirect(response):
     if response.history:
         raise ErrRedirection("Redirection")
 
 
-def parse_book_page(page_content):
+def parse_book_page(page_content, book_id):
     soup = BeautifulSoup(page_content, 'lxml')
     author_and_name = soup.find('h1').text
     book_name, author = author_and_name.split('::')
-    return author, book_name
+
+    covers_url = urllib.parse.urljoin(url, soup.find('div', class_='bookimage').find('img')['src'])
+    file_path_comments = f'comments/comment_{book_id}.txt'
+    file_path_genres = f'genres/genre_{book_id}.txt'
+    genres = soup.find_all('span', class_='d_book')
+    genres = [genre.text for genre in genres]
+    with open(file_path_genres, 'w', encoding='UTF-8') as file:
+        file.write('\n'.join(genres))
+    comments = soup.find_all('span', class_='black')
+    comments = [comment.text for comment in comments]
+    with open(file_path_comments, 'w', encoding='UTF-8') as file:
+        file.write('\n'.join(comments))
+    return author, book_name, covers_url
 
 
 def download_book(book_id, file_path):
@@ -28,51 +45,47 @@ def download_book(book_id, file_path):
     with open(file_path, 'wb') as file:
         file.write(response.content)
 
-
-def make_path(filename):
-    folder = 'books'
-    file_path = os.path.join(folder, filename)
-    return file_path
-
+def covers_download(covers_url, book_id):
+    response = requests.get(covers_url)
+    response.raise_for_status()
+    file_path_covers = f'covers/cover_{book_id}.jpg'
+    with open(file_path_covers, 'wb') as file:
+        file.write(response.content)
 
 if __name__ == '__main__':
     os.makedirs('books', exist_ok=True)
     os.makedirs('covers', exist_ok=True)
     os.makedirs('comments', exist_ok=True)
     os.makedirs('genres', exist_ok=True)
-    start_id = int(input('Введите первое число:  '))
-    end_id = int(input('Введите второе число:  '))
-    for i in tqdm(range(10000)):
-        for book_id in range(start_id, end_id):
-            try:
-                url = f'https://tululu.org/b{book_id}/'
-                response = requests.get(url)
-                response.raise_for_status()
-                print(response.history)
-                check_for_redirect(response)
-                page_content = response.text
-                soup = BeautifulSoup(page_content, 'lxml')
-                print(soup.find('bookimage'))
-                covers_url = urllib.parse.urljoin('https://tululu.org/', soup.find('div', class_='bookimage').find('img')['src'])
-                file_path2 = f'comments/comment_{book_id}.txt'
-                file_path4 = f'genres/genre_{book_id}.txt'
-                genres = soup.find_all('span',  class_='d_book')
-                genres = [genre.text for genre in genres]
-                with open(file_path4, 'w', encoding='UTF-8') as file:
-                    file.write('\n'.join(genres))
-                comments = soup.find_all('span', class_='black')
-                comments = [comment.text for comment in comments]
-                with open(file_path2, 'w', encoding='UTF-8') as file:
-                    file.write('\n'.join(comments))
-
-                response = requests.get(covers_url)
-                response.raise_for_status()
-                file_path3 = f'covers/cover_{book_id}.jpg'
-                with open(file_path3, 'wb') as file:
-                    file.write(response.content)
-                author, book_name = parse_book_page(page_content)
-                filename = f'{book_name}.txt'
-                file_path = make_path(filename)
-                download_book(book_id, file_path)
-            except ErrRedirection:
-                print('Перенаправление')
+    parser = argparse.ArgumentParser(
+        description='Данная программа скачивает книги'
+    )
+    start_id = parser.add_argument("start_id", help="first number", type=int)
+    end_id = parser.add_argument("end_id", help="second number", type=int)
+    args = parser.parse_args()
+    for book in tqdm(range(args.end_id)):
+        for book_id in range(args.start_id, args.end_id):
+            while True:
+                try:
+                    url = f'https://tululu.org/b{book_id}/'
+                    response = requests.get(url)
+                    response.raise_for_status()
+                    check_for_redirect(response)
+                    page_content = response.text
+                    soup = BeautifulSoup(page_content, 'lxml')
+                    covers_url = urllib.parse.urljoin(url, soup.find('div', class_='bookimage').find('img')['src'])
+                    parse_book_page(page_content, book_id)
+                    covers_download(covers_url, book_id)
+                    author, book_name, covers_url = parse_book_page(page_content, book_id)
+                    file_path = f'books/{book_name}.txt'
+                    download_book(book_id, file_path)
+                    break
+                except ErrRedirection:
+                    logging.warning("Перенаправление")
+                    break
+                except requests.exceptions.ConnectionError:
+                    logging.warning("Потеряно соединение")
+                    time.sleep(4)
+                except requests.exceptions.HTTPError:
+                    logging.exception("Ошибка при запросе")
+                    break
